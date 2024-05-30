81ea7e76a51bde407f9b21809b869e186e0f32c5
Andrew Chan <09ychan@brightoncollege.net>
2024-05-30 16:18:59 +0100

    fixed an error in Crisp code

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import {Crisp} from "crisp-sdk-web";
 
 export const CrispChat = () => {
     useEffect(() => {
-        Crisp.configure("4e7bee51-2310-4851-b668-f153cd42f88ac");
+        Crisp.configure("4e7bee51-2310-4851-b668-f153cd42f88a");
     }, []);
 
     return null;
